20a65d4827663fc13cf67ca941ea9d9d98a68b9e
indraworks <indra.works2020@gmail.com>
2021-04-14 16:24:47 +0700

    first commit for all re-new

---
+++ brad_smilga/test01/app.js
@@ -0,0 +1,11 @@
+const x = document.location.hash;
+const checkUrl = x.split('/');
+const hasil = checkUrl[0];
+const hasil2 = checkUrl[1];
+const hasil3 = checkUrl[2];
+
+console.log(checkUrl);
+console.log(x);
+console.log();
+console.log(hasil2);
+console.log(hasil3);
